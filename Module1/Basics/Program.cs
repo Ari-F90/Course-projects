5b62501ae215d87d90d12cc12db963ad3e319364
Ariadna Frias <ariadna.812@gmail.com>
2023-09-23 17:43:19 +0200

    Added basic exercises

---
+++ Module1/Basics/Program.cs
@@ -0,0 +1,11 @@
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write your name");
+string input = Console.ReadLine();
+Console.WriteLine($"Hello,{input}");
+
+Console.WriteLine("Write your name");
+string username = Console.ReadLine();
+Console.WriteLine("Write your city");
+string city = Console.ReadLine();
+Console.WriteLine($"Hello,{username}, welcome to {city}");
