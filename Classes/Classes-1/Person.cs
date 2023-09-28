75a92531b2dd496cde4ab63b8672f26e53d4df09
Ariadna Frias <ariadna.812@gmail.com>
2023-09-28 11:30:42 +0200

    Added first ex in class module

---
+++ Classes/Classes-1/Person.cs
@@ -0,0 +1,11 @@
+
+namespace Classes
+{
+  public class Person
+{
+  public string Username {get; set;}
+  public int Age {get; set;}
+}
+}
+
+
---
+++ Classes/Classes-1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < 5; i++)
+        {
+          Console.WriteLine("Introduce a name");
+          var username = Console.ReadLine();
+          Console.WriteLine("Introduce an age");
+          var age = int.Parse(Console.ReadLine());
+           people.Add(new Person
+           {
+            Username = username,
+            Age = age
+           })
+        }
+        foreach (person in people)
+        {
+          if (person.Age >= 18)
+          {
+            Console.WriteLine($"The name of this person is {person.Username}");
+          }
+        }
+
+    }
+}
+
+
+
+
+
+
+
