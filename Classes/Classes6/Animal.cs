cb1eead5e1d4844be46bbccadaa703d7086c1692
Ariadna Frias <ariadna.812@gmail.com>
2023-09-28 15:46:15 +0200

    Added 6th ex on class module

---
+++ Classes/Classes6/Animal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Classes6
+{
+  public class Animal
+  {
+    public string Type { get; set; }
+    public string HairColor { get; set; }
+    public bool IsDomestic { get; set; }
+    public int Legs { get; set; }
+  }
+}
---
+++ Classes/Classes6/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes4
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+      Animal cat = new Animal{
+        Type = 'cat',
+        HairColor = 'brown',
+        IsDomestic = true,
+        Legs = 4,
+      };
+
+      Animal elephant = new Animal();
+      elephant.Type = "elephant";
+      elephant.HairColor = "grey";
+      elephant.IsDomestic = false;
+      elephant.Legs = 4;
+    }
+  }
+}
+
