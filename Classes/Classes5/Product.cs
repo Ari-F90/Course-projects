59203154196b42a56a72dcf6a46ead0b15061165
Ariadna Frias <ariadna.812@gmail.com>
2023-09-28 15:21:35 +0200

    Added 5th ex in module class

---
+++ Classes/Classes5/Product.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Classes5
+{
+  public class Product
+  {
+    public static int SumProducts(int num1, int num2)
+    {
+      return num1 + num2;
+    }
+  }
+}
---
+++ Classes/Classes5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes4
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+      int sum = Product.SumProducts(43,67);
+    }
+  }
+}
+
